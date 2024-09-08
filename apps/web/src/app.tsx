c881a361005f9c734e287ec5adadd083da1c2ed1
Prajyot Tayde <pmtbmt@gmail.com>
2024-09-08 16:03:13 +0530

    setup react-router-dom

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,23 +1,19 @@
 import { useEffect, useState } from "react";
 import { Button } from "@growfuse/ui/components/button";
+import { Route, Routes } from "react-router-dom";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   const [count, setCount] = useState(2);
 
   return (
-    <div className="mx-auto p-5">
-      <h1 className="text-xl font-bold mb-2">This is a Vite application</h1>
-      <p className="mb-4">
-        This shadcn/ui button is shared between Vite, NextJS and any other
-        application.
-      </p>
-      <Button
-        variant={"destructive"}
-        onClick={() => setCount((count) => count + 1)}
-      >
-        Count is {count}
-      </Button>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/page1" element={<Page1 />} />
+      <Route path="/page2" element={<Page2 />} />
+    </Routes>
   );
 }
 
---
+++ apps/web/src/pages/Page1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page1 = () => {
+  return (
+    <div className='w-full h-screen bg-gray-400 text-white'>Page1</div>
+  )
+}
+
+export default Page1
